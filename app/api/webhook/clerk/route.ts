f71e975fd0a4987f9904e1cdedc9c11473547cba
sonu <srssoumyaranjansahu123@gmail.com>
2024-01-24 23:52:29 +0530

    commit

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -61,8 +61,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName:last_name,
+      firstName:'admin',
+      lastName:'admin',
       photo: image_url,
     }
 
